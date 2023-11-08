2185dade759f3d913c90f410e216efdbea0bf089
josego16 <jgomlin@g.educaand,es>
2023-11-08 19:10:51 +0100

    jugando con los linearLayout

--- app/src/main/java/com/example/interfaz_usuario/Linear_Anidado_Activity.kt
+++ app/src/main/java/com/example/interfaz_usuario/Linear_Anidado_Activity.kt
@@ -3,9 +3,9 @@ package com.example.interfaz_usuario
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LinearActivity : AppCompatActivity() {
+class Linear_Anidado_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_linear)
+        setContentView(R.layout.activity_linear_anidado)
     }
 }
