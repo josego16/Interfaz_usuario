0c8a81a3394ebf6d0cc229f401add78e34e7425d
josego16 <jgomlin@g.educaand.es>
2023-11-10 22:35:17 +0100

    hemos creado un xml para el tableLayout

---
+++ app/src/main/java/com/example/interfaz_usuario/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.interfaz_usuario
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_activity)
+    }
+}
